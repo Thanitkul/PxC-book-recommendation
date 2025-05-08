417b8c05c8fd91c8478e2f2b7054e9a11add1df0
Phasit Thanitkul_Kane <kanephasit@gmail.com>
2025-05-09 00:34:04 +0700

    debug

--- recsys_server/recsys/logic/cold_start.py
+++ recsys_server/recsys/logic/cold_start.py
@@ -1,5 +1,6 @@
 from sentence_transformers import util
 from recsys.data.feature_store import feature_store
+import torch
 
 
 def recommend_by_genres(user_genres: str, top_n=10):
@@ -22,7 +23,9 @@ def recommend_by_genres(user_genres: str, top_n=10):
     user_embedding = genre_embeddings.mean(dim=0)
     
     # Compute cosine similarity between the user profile and all book embeddings
+    noise_strength = 0.02  # tune between 0.01–0.05 for subtle randomness
     scores = util.pytorch_cos_sim(user_embedding, feature_store.book_tag_embeddings)[0]
+    scores += noise_strength * torch.randn_like(scores)
     top_results = scores.topk(top_n)
     
     # Retrieve the top matching books
